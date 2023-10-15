8e51eb79771aa28be5d21b97bed5a36f4c3ea620
Walter Kimaro <wakyj07@gmail.com>
2023-10-15 19:45:43 +0300

    Initial setup

---
+++ brandy.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  Plugin Name: Brandy
+  Plugin URI:
+  Description: Brandy
+  Version: 1.0.0
+  Author: Walter Kimaro
+  Author URI: https://github.com/jestrux
+  License:
+ */
+
+if (!defined('ABSPATH')) exit;
+
+require_once(__DIR__ . '/class-create-admin-page.php');
